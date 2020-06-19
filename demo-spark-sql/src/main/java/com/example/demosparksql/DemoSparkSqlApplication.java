64016bc3dbd86c2d8d6f29977893965f7955f8b8
tshch1989 <tshch0115>
2020-06-19 15:19:16 +0800

    first commit

---
+++ demo-spark-sql/src/main/java/com/example/demosparksql/DemoSparkSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosparksql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSparkSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSparkSqlApplication.class, args);
+    }
+
+}
---
+++ demo-spark-sql/src/test/java/com/example/demosparksql/DemoSparkSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosparksql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSparkSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
